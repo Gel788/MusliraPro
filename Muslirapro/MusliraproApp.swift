c5cc0bbcdc4e51ba293d4520d927b4396b5afa7f
Albert Giloyan <albertgiloan@gmail.com>
2025-09-22 13:15:34 +0300

    Initial Commit

---
+++ Muslirapro/MusliraproApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusliraproApp.swift
+//  Muslirapro
+//
+//  Created by Альберт Гилоян on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusliraproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusliraproTests/MusliraproTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusliraproTests.swift
+//  MusliraproTests
+//
+//  Created by Альберт Гилоян on 22.09.2025.
+//
+
+import Testing
+@testable import Muslirapro
+
+struct MusliraproTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
